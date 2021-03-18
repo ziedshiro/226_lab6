a5ecd5fd49f48ab76e7e058635b98aa804e1209c
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-18 17:37:30 +0700

    1

--- 6320502991_6_6.c
+++ 6320502991_6_6.c
@@ -2,10 +2,43 @@
 #include<string.h>
 int main()
 {
-    int n,i,j;
+    int n,i,j,x=0,y=0,k=0;
     scanf("%d",&n);
-    char s[n][n];
+    char s[n][n],S[250];
     for(i=0;i<n;i++)
         for(j=0;j<n;j++)
             s[i][j]='.';
+    scanf("%s",S);
+    if(S[0]=='R')
+    {
+        s[0][0]='-';
+        x++;
+    }
+    else
+    {
+        s[0][0]='|';
+        y++;
+    }
+    for(i=1;i<strlen(S);i++)
+    {
+        if(S[i]=='U'||S[i]=='D')
+        {
+            s[x][y]='|';
+            if(k>0&&S[i]=='U')
+                x--;
+            else if(k>0&&S[i]=='D')
+                x++;
+
+
+        }
+        else
+        {
+            s[x][y]='-';
+            s[x][y]='|';
+            if(k>0&&S[i]=='R')
+                y++;
+            else if(k>0&&S[i]=='L')
+                y--;
+        }
+    }
 }
