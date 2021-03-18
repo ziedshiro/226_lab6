ec8419b4193beadc7107205ed310a5acb009de7d
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-18 15:58:11 +0700

    5

--- 6320502991_6_5.c
+++ 6320502991_6_5.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int n,m,i,j;
+    int n,m,i,j,r=0;;
     scanf("%d %d",&m,&n);
+    int s[n][m];
+    for(i=0;i<n;i++)
+        for(j=0;j<m;j++)
+        scanf("%d",&s[i][j]);
+    for(i=0;i<n;i++)
+        for(j=0;j<m;j++)
+                r+=s[i][j];
+    printf("%d",(m*n)-(r%(m*n)));
 }
