e03e318d07dcad4cc8ed16a02806585e6dc6fa25
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-18 14:57:49 +0700

    1

--- 6320502991_6_1.c
+++ 6320502991_6_1.c
@@ -2,10 +2,18 @@
 #include<string.h>
 int main()
 {
-    int n,i;
+    int n,i,j;
     scanf("%d",&n);
     char s[n][n];
     for(i=0;i<n;i++)
         scanf("%s",s[i]);
-    printf("%c",s[1][1]);
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+        {
+            if(s[i][j]>='a'&&s[i][j]<='z')
+            {
+                s[i][j]-='a'-'A';
+                printf("%c",s[i][j]);
+            }
+        }
 }
