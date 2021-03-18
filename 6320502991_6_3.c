75236118626bad90ed2f635ac749595d6782f766
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-18 16:17:55 +0700

    3

--- 6320502991_6_3.c
+++ 6320502991_6_3.c
@@ -3,5 +3,14 @@ int main()
 {
     int n,q;
     scanf("%d %d",&n,&q);
+    int s[n],i,a[q],b[q],c[q];
+    for(i=0;i<n;i++)
+        s[i]=0;
+    for(i=0;i<q;i++)
+    {
+        scanf("%s",&a[i]);
+        scanf("%d",&b[i]);
+        scanf("%d",&c[i]);
+    }
 
 }
