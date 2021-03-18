14b1ef18bceb84e964de232bd4221961e70e4b36
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-18 16:54:01 +0700

    3

--- 6320502991_6_3.c
+++ 6320502991_6_3.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
     int n,q,j,max;
     scanf("%d %d",&n,&q);
-    int s[n],i,a[q],b[q],c[q];
+    int s[n],i,b[q],c[q];
+    char a[q];
     for(i=0;i<n;i++)
         s[i]=0;
     for(i=0;i<q;i++)
@@ -14,17 +16,21 @@ int main()
     }
     for(i=0;i<q;i++)
     {
+
         if(a[i]=='U')
+        {
             s[b[i]-1]=c[i];
+        }
         else if(a[i]=='P')
         {
             max=s[b[i]-1];
+
             for(j=b[i]-1;j<c[i];j++)
             {
                 if(max<s[j])
                     max=s[j];
             }
-            printf("%d\n",max);
+        printf("%d\n",max);
         }
     }
 }
