bc993aaa0d0af20000a5557e69aec80febe804fa
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-18 15:18:20 +0700

    2

--- 6320502991_6_2.c
+++ 6320502991_6_2.c
@@ -2,11 +2,12 @@
 #include<string.h>
 int main()
 {
-    int n,i;
+    int n,i,j,r=0;
     scanf("%d",&n);
     char N[2][n];
     for(i=0;i<2;i++)
         scanf("%s",N[i]);
-        for(i=0;i<n;i++)
-    printf("%c",N[0][i]);
+    for(i=0;i<n;i++)
+          if(N[0][i]==N[1][i])
+                r++;
 }
