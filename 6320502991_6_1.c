1ce82156d5d5110014f7a9ae9df58d779353a085
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-18 15:09:24 +0700

    1

--- 6320502991_6_1.c
+++ 6320502991_6_1.c
@@ -2,7 +2,7 @@
 #include<string.h>
 int main()
 {
-    int n,i,j;
+    int n,i,j,sw;
     scanf("%d",&n);
     char s[n][n],S[n][n];
     int r[n];
@@ -15,4 +15,18 @@ int main()
             if(s[i][j]>='a'&&s[i][j]<='z')
                 S[i][j]-='a'-'A';
         }
+    for(i=0;i<n;i++)
+        r[n]=i;
+    for(i=0;i<n-1;i++)
+        for(j=0;j<n-1;j++)
+        {
+            if(S[j][0]<S[j+1][0])
+                {
+                   sw=r[j];
+                   r[j]=r[j+1];
+                   r[j+1]=sw;
+                }
+        }
+    for(i=0;i<n;i++)
+        printf("%s",s[r[i]]);
 }
