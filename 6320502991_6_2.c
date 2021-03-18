0a74f149533c790f8d4f6a19cd82255a64fc6381
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-18 15:14:54 +0700

    2

--- 6320502991_6_2.c
+++ 6320502991_6_2.c
@@ -2,5 +2,11 @@
 #include<string.h>
 int main()
 {
-
+    int n,i;
+    scanf("%d",&n);
+    char N[2][n];
+    for(i=0;i<2;i++)
+        scanf("%s",N[i]);
+        for(i=0;i<n;i++)
+    printf("%c",N[0][i]);
 }
