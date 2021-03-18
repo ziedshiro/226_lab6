a1df7c82294f4b902202b456a81cfa36911e8f61
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-18 17:08:23 +0700

    1

--- 6320502991_6_1.c
+++ 6320502991_6_1.c
@@ -2,31 +2,34 @@
 #include<string.h>
 int main()
 {
-    int n,i,j,sw;
+    int n,i,j,sw,x;
     scanf("%d",&n);
     char s[n][n],S[n][n];
     int r[n];
     for(i=0;i<n;i++)
         scanf("%s",s[i]);
     for(i=0;i<n;i++)
-        for(j=0;j<n;j++)
         {
-            S[i][j]=s[i][j];
-            if(s[i][j]>='a'&&s[i][j]<='z')
-                S[i][j]-='a'-'A';
+            S[i][0]=s[i][0];
+            if(s[i][0]>='a'&&s[i][0]<='z')
+                S[i][0]-='a'-'A';
         }
     for(i=0;i<n;i++)
-        r[n]=i;
+        r[i]=i;
     for(i=0;i<n-1;i++)
         for(j=0;j<n-1;j++)
         {
-            if(S[j][0]<S[j+1][0])
+            if(S[j][0]>S[j+1][0])
                 {
-                   sw=r[j];
-                   r[j]=r[j+1];
-                   r[j+1]=sw;
+                    x=S[j][0];
+                    S[j][0]=S[j+1][0];
+                    S[j+1][0]=x;
+                    x=r[j];
+                    r[j]=r[j+1];
+                    r[j+1]=x;
                 }
         }
+
     for(i=0;i<n;i++)
-        printf("%s",s[r[i]]);
+        printf("%s\n",s[r[i]]);
 }
