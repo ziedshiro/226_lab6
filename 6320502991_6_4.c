d66b9fb59c9af380a5120e719e2804a7333add97
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-18 15:26:50 +0700

    4

--- 6320502991_6_4.c
+++ 6320502991_6_4.c
@@ -1,10 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int n;
+    int n,k;
     scanf("%d",&n);
-    int i,j,s[n][2];
+    int i,j,s[n][2],r[n];
+    for(i=0;i<n;i++)
+        r[n]=0;
     for(i=0;i<n;i++)
         for(j=0;j<2;j++)
         scanf("%d",&s[i][j]);
+    for(k=0;k<n;k++)
+    for(i=0;i<n;i++)
+        for(j=0;j<2;j++)
+    {
+
+
+    }
 }
